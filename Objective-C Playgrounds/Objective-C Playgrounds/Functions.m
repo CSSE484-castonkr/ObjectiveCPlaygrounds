c7c9f63fc6bdf59a14629eeccd379138c3606a0a
Kiana Caston <castonkr@rose-hulman.edu>
2018-03-26 11:21:00 -0400

    functions done

--- Objective-C Playgrounds/Objective-C Playgrounds/Functions.m
+++ Objective-C Playgrounds/Objective-C Playgrounds/Functions.m
@@ -11,7 +11,43 @@
 @implementation Functions
 
 + (void) run {
+    NSLog(@"Your fortune... %@", [Functions fortuneCookie]);
+    NSLog(@"Your fortune... %@", [Functions fortuneCookie]);
+    
+    Functions* f = [[Functions alloc] init];
+    
+    NSLog(@"The Magic 8 Ball says... %@", [f magic8Ball]);
+    NSLog(@"The Magic 8 Ball says... %@", [f magic8Ball]);
+    
+    NSLog(@"Superhero name: %@", [f superheroName:@"Kiana"]);
+    NSLog(@"Superhero name: %@", [f superheroName:@"Batman"]);
+    
+    NSLog(@"Volume for (3, 3, 3): %f", [Functions calculateVolumeForLength: 3
+                                                                  forWidth: 3
+                                                                 forHeight: 3]);
+    NSLog(@"Volume for (1.5, 2, 1): %f", [Functions calculateVolumeForLength: 1.5
+                                                                    forWidth: 2
+                                                                   forHeight: 1]);
 
 }
+
++ (NSString*) fortuneCookie{
+    int numFriends = (int)arc4random_uniform(300);
+    return [NSString stringWithFormat:@"\nYou will gain %d friends on Facebook today!", numFriends];
+}
+
+- (NSString*) magic8Ball{
+    int numFriends = (int)arc4random_uniform(5);
+    return [NSString stringWithFormat:@"\nYou will gain %d friends on Google+ today!", numFriends];
+}
+
+- (NSString*) superheroName: (NSString*) name{
+    return [NSString stringWithFormat:@"Super %@", [name uppercaseString]];
+}
+
++ (double) calculateVolumeForLength: (double) length forWidth: (double) width forHeight: (double) height{
+    return length * width * height;
+}
+
 
 @end
